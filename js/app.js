1ea6ad9a70fbdf3e98d887b115a79519ef467b7c
Devlux <devlux@gemspace.net>
2016-04-20 04:02:39 -0500

    Prepping for release

--- js/app.js
+++ js/app.js
@@ -1,44 +1,85 @@
 (function() {
   'use strict';
 
-  angular.module('slackChatApp', ['valortech.slackChat']);
+    function SlackCtrl($scope, slackSvc) {
 
-  angular.module('slackChatApp').controller('Shell', Shell);
+        var clientId = "18851227428.35732291009";
+        var clientSecret = "be753b4edbcbe82389519e6ddb4f240c";
+        var vm = this;
+        var config = {
+            client: clientId,
+            authParams : {
+                team : "nxtchat",
+                state : NRS.accountRS,
+                redirect_uri : "http://localhost:7876/plugins/slackchat/html/pages/return.html",
+                scope : "identify channels:read files:read channels:write"
+            }
+        };
 
-  function Shell() {
+        vm.messages = [
+            {
+                'username': 'Welcome Bot',
+                'content': 'Logging you into slack'
+            }
+        ];
 
-    var vm = this;
+        vm.onmessage = function(message){
+            vm.messages.push({
+                'username' : message.user,
+                'content' : message.text
+            });
+        };
+        vm.sendMessage = function(message, username) {
+            if(message && message !== '' && username) {
+                vm.messages.push({
+                    'username': username,
+                    'content': message
+                });
+                slackSvc.chat.postMessage($scope,$scope.channelId,message,$scope.token);
+            }
+        };
 
-    vm.messages = [
-      {
-        'username': 'Matt',
-        'content': 'Hi!'
-      },
-      {
-        'username': 'Elisa',
-        'content': 'Whats up?'
-      },
-      {
-        'username': 'Matt',
-        'content': 'I found this nice NXT plugin'
-      },
-      {
-        'username': 'Elisa',
-        'content': 'Looks Great!'
-      }
-    ];
+        vm.onRTMStart = function(msg){
+            var url = msg.url;
+            $scope.socket = new WebSocket(url);
+            $scope.socket.onopen = function(event){
+                if(!event.error) {
+                    vm.messages.push({'username': 'Welcome Bot', 'content': 'Successfully logged in'});
+                }else{
+                    console.error("Error connecting: ",event);
+                }
+            };
 
-    vm.username = 'Matt';
+            $scope.socket.onmessage = function(msg){
+                var data = JSON.parse(msg);
+                if(data.type == "message"){
+                    vm.messages.push({'username': data.user, 'content': data.text});
+                }else{
+                    console.warn("Unhandled Message Type: ",data);
+                }
+            }
+        };
 
-    vm.sendMessage = function(message, username) {
-      if(message && message !== '' && username) {
-        vm.messages.push({
-          'username': username,
-          'content': message
-        });
-      }
-    };
+        var token = localStorage.getItem("slack.token");
+        if(!token){
+            slackSvc.authorize(config.client,config.authParams);
+        }else{
+            console.log("Authing: ",NRS.accountRS);
+            slackSvc.oauth.access(clientId, clientSecret,token , function (response) {
+                if(response.ok){
+                    //optional : preload you token for further requests
+                    slackSvc.InitToken(response.access_token);
+                    slackSvc.rtm.start(response.access_token,vm.onRTMStart);
+                }else{
+                    console.log("Error in slack auth: ",response);
+                }
+            });
+        }
 
-  }
+    }
+    angular.module('slackChatApp', [
+        'Deg.SlackApi',
+        'valortech.slackChat'
+    ]).controller('SlackCtrl', ['$scope', 'slackSvc',SlackCtrl]);
 
 })();
